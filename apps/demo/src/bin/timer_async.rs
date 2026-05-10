027c18e01b00ad92924bf753f7a9cd74d63cb847
Oveln <oveln@outlook.com>
2026-05-11 05:41:01 +0800

    demo: 添加 timer_async 示例
    使用 futures::timer::after() 每秒输出日志，循环 5 次后关机。

---
+++ apps/demo/src/bin/timer_async.rs
@@ -0,0 +1,31 @@
+//! 演示 `futures::timer::after()` 的基本用法
+//!
+//! 每秒输出一条日志，循环 5 次后关机。
+
+#![no_std]
+#![no_main]
+#![feature(impl_trait_in_assoc_type)]
+
+use executor::priority::Priority;
+use fugit::ExtU64;
+use platform::platform_traits::Chip;
+
+#[executor::task]
+async fn tick_task() {
+    for i in 1..=5 {
+        futures::timer::after(1.secs()).await;
+        log::info!("tick #{i}");
+    }
+    log::info!("timer_async demo done");
+    platform::ChipImpl::shutdown();
+}
+
+#[executor::interrupt]
+fn MachineTimer(_tf: &mut platform::arch::TrapFrame) {
+    futures::timer::handle_timer_isr();
+}
+
+#[executor::main]
+fn main(spawner: core::pin::Pin<&'static executor::spawner::Spawner<4>>) {
+    spawner.spawn(Priority::new(0), tick_task().unwrap());
+}
